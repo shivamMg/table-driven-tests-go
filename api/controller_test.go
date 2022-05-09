c8b385859caf7c9ff4447bc9ff5d787bf33fc055
Shivam Mamgain <shivamMg@users.noreply.github.com>
2022-05-09 17:57:18 +0530

    More tests + GH Actions

--- api/controller_test.go
+++ api/controller_test.go
@@ -2,6 +2,7 @@ package api_test
 
 import (
 	"bytes"
+	"errors"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -21,6 +22,89 @@ func assertEqual(t *testing.T, expected any, actual any) {
 		return
 	}
 	t.Fatalf("%v (expected) != %v (actual)", expected, actual)
+}
+
+func TestController_CreateTODO_UnauthenticatedError(t *testing.T) {
+	c := gomock.NewController(t)
+	defer c.Finish()
+	auth := mock.NewMockAuthenticator(c)
+	db := mock.NewMockDatabase(c)
+	ctrl := api.NewController(auth, db)
+
+	w := httptest.NewRecorder()
+	rBody := bytes.NewBufferString(`{"name": "task1", "category": "cat1"}`)
+	r := httptest.NewRequest(http.MethodPost, "http://example.com/todos", rBody)
+	r.Header.Add("AuthToken", testToken)
+
+	auth.EXPECT().IsAuthenticated(testToken).Return(false)
+
+	ctrl.CreateTODO(w, r)
+
+	resp := w.Result()
+	respBody, _ := io.ReadAll(resp.Body)
+
+	assertEqual(t, 401, resp.StatusCode)
+	assertEqual(t, "unauthenticated\n", string(respBody))
+}
+
+func TestController_CreateTODO_BadRequestErrors(t *testing.T) {
+	testCases := []struct {
+		name             string
+		requestBody      string
+		expectedResponse string
+	}{
+		{"invalid json", `{"name"}`, "invalid json: invalid character '}' after object key\n"},
+		{"empty name", `{"name": ""}`, "invalid todo: empty name\n"},
+		{"empty category", `{"name": "task1", "category": ""}`, "invalid todo: empty category\n"},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			c := gomock.NewController(t)
+			defer c.Finish()
+			auth := mock.NewMockAuthenticator(c)
+			db := mock.NewMockDatabase(c)
+			ctrl := api.NewController(auth, db)
+
+			w := httptest.NewRecorder()
+			rBody := bytes.NewBufferString(tc.requestBody)
+			r := httptest.NewRequest(http.MethodPost, "http://example.com/todos", rBody)
+			r.Header.Add("AuthToken", testToken)
+
+			auth.EXPECT().IsAuthenticated(testToken).Return(true)
+
+			ctrl.CreateTODO(w, r)
+
+			resp := w.Result()
+			respBody, _ := io.ReadAll(resp.Body)
+
+			assertEqual(t, 400, resp.StatusCode)
+			assertEqual(t, tc.expectedResponse, string(respBody))
+		})
+	}
+}
+
+func TestController_CreateTODO_DBError(t *testing.T) {
+	c := gomock.NewController(t)
+	defer c.Finish()
+	auth := mock.NewMockAuthenticator(c)
+	db := mock.NewMockDatabase(c)
+	ctrl := api.NewController(auth, db)
+
+	w := httptest.NewRecorder()
+	rBody := bytes.NewBufferString(`{"name": "task1", "category": "cat1"}`)
+	r := httptest.NewRequest(http.MethodPost, "http://example.com/todos", rBody)
+	r.Header.Add("AuthToken", testToken)
+
+	auth.EXPECT().IsAuthenticated(testToken).Return(true)
+	db.EXPECT().CreateTODO(&api.TODO{"task1", "cat1"}).Return(errors.New("failed to commit txn"))
+
+	ctrl.CreateTODO(w, r)
+
+	resp := w.Result()
+	respBody, _ := io.ReadAll(resp.Body)
+
+	assertEqual(t, 500, resp.StatusCode)
+	assertEqual(t, "db error: failed to commit txn\n", string(respBody))
 }
 
 func TestController_CreateTODO_Success(t *testing.T) {
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/shivamMg/table-driven-tests-go
 
 go 1.18
 
-require github.com/golang/mock v1.6.0 // indirect
+require github.com/golang/mock v1.6.0
